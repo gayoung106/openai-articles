201e8b0503a702b08639dfeb72367bf85668b14c
SongGayoung <xjongam1x@gmail.com>
2023-09-20 17:07:32 +0900

    검색창: input 창

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+
 const Demo = () => {
-  return <div>Demo</div>;
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+        >
+          <img src={linkIcon} className="absolute left-0 my-2 ml-3 w-5" />
+
+          <input
+            type="url"
+            placeholder="URL을 입력하세요."
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer"
+          />
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
